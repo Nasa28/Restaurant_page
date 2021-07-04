5d14fc9c6088401b1d17694365a87e93328614b8
Nasa28 <kaluchinasaagu@gmail.com>
2021-07-04 12:29:56 +0100

    add background image

---
+++ src/home.js
@@ -0,0 +1,10 @@
+const home = () => {
+  const main = document.createElement('div');
+  const h3 = document.createElement('h3');
+  const p = document.createElement('p');
+  const image = document.createElement('img')
+
+  main.classList.add('main');
+
+  
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 
 import myNav from './navbar';
+import home from './home';
 
 const content = document.getElementById('content');
   myNav();
+  home()
