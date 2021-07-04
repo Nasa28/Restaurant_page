7efc27bd46b51386767ee8b7d2750189550c330f
Nasa28 <kaluchinasaagu@gmail.com>
2021-07-04 14:08:14 +0100

    style home.js

--- src/home.js
+++ src/home.js
@@ -4,6 +4,10 @@ const home = () => {
   const p = document.createElement('p');
   const imageParent = document.createElement('div')
   const image = document.createElement('img')
+  const detail = document.createElement('p')
+  const btn = document.createElement('h4')
+  const a = document.createElement('a');
+  
   image.id = "id";
   image.className = "image";
   image.src = '../food-13642.html'
@@ -12,11 +16,20 @@ const home = () => {
   imageParent.classList.add("image_parent")
   h3.classList.add('h3')
   p.classList.add('p')
+  detail.classList.add('detail')
   h3.textContent= "Welcome to Chop-life- restaurant.";
   p.textContent = `We are always there to serve you better.
-  you will never regret it`;
-
+  you will never regret it. 
+  `;
+  detail.textContent = `
+  Check our menu list to place your order now!!!`
+  btn.textContent = 'ORDER'
+  a.setAttribute('href', '#');
+  a.appendChild(btn)
   imageParent.appendChild(image);
+  imageParent.appendChild(detail);
+  detail.appendChild(a)
+ 
   p.appendChild(imageParent);
   h3.appendChild(p);
   main.appendChild(h3);
